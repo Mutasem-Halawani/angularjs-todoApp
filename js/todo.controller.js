317fff84df71b8877c87cd48230307aacd1f446d
mutasem <mutasem.halawani@gmail.com>
2018-09-17 14:11:54 +0300

    add add/remove/edit options to Todo

--- js/todo.controller.js
+++ js/todo.controller.js
@@ -1,8 +1,19 @@
 function TodoController () {
+  this.newTodo = ''
+  this.addTodo = function () {
+    this.list.unshift({
+      title: this.newTodo,
+      completed: false
+    })
+    this.newTodo = ''
+  }
+  this.removeTodo = function (item, index) {
+    this.list.splice(index, 1)
+  }
   this.list = [
     {
       title: 'First todo item!',
-      completed: false
+      completed: true
     },
     {
       title: 'Second todo item!',
